219632eb59c50451079aaf33056df50886eea593
Julius <julius.kurvers@gmail.com>
2026-03-22 14:07:16 +0100

    upon dragging, check for existing files and append

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -68,7 +68,8 @@
           />
 
           <v-file-input
-            v-model="form.files"
+            :model-value="form.files"
+            @update:modelValue="onFilesChange"
             color="primary"
             :label="$t('home.form.files')"
             :rules="rules.files"
@@ -209,8 +210,12 @@ export default defineComponent({
     }
   },
   methods: {
-    async submit() {
+    onFilesChange(newFiles: File[] | File) {
+      const incoming = Array.isArray(newFiles) ? newFiles : [newFiles]
+      this.form.files = [...this.form.files, ...incoming]
+    },
 
+    async submit() {
       if(!this.form.checked || this.form.files.length == 0) {
         this.error = this.$t("home.invalidFieldsError");
         window.scroll({
